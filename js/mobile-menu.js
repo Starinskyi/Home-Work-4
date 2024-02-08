db90bf94162148a1e4cab14e76beddae1bd32033
Oleksanfr <oleksandrstarinskyi@gmail.com>
2024-02-08 19:52:02 +0100

    update

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,10 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuBtnOpen = document.querySelector(".menu-btn-open");
 const menuBtnClose = document.querySelector(".menu-btn-close");
+const mobileMenuLink = document.getElementById("mobile-link-1");
+const mobileMenuLink2 = document.getElementById("mobile-link-2");
+const mobileMenuLink3 = document.getElementById("mobile-link-3");
+const mobileMenuLink4 = document.getElementById("mobile-link-4");
 
 const toggleMenu = () => mobileMenu.classList.toggle("is-open");
 const disableScroll = () =>
@@ -11,3 +15,8 @@ menuBtnClose.addEventListener("click", toggleMenu);
 
 menuBtnOpen.addEventListener("click", disableScroll);
 menuBtnClose.addEventListener("click", disableScroll);
+
+mobileMenuLink.addEventListener("click", toggleMenu);
+mobileMenuLink2.addEventListener("click", toggleMenu);
+mobileMenuLink3.addEventListener("click", toggleMenu);
+mobileMenuLink4.addEventListener("click", toggleMenu);
